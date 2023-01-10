d1a01e3b65b655b1cad67f2bf46d4491dca66796
Zaw Myo Htun <jzhtun@gmail.com>
2023-01-10 21:18:01 +0630

    Controller - done, Docker - initial

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,16 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddScoped<ICatalogContext,CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
